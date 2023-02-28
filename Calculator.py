4f4b3d57389abc8d7928956042416edeabc87d73
Mitchell Blaha <99313238+WhiteRabbit2006@users.noreply.github.com>
2023-02-28 13:49:16 -0500

    Update Calculator.py
    minor changes

--- Calculator.py
+++ Calculator.py
@@ -2,23 +2,30 @@ import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
-# Railgun and projectile construction
-d = 0.00635  # separation of the rails and width of the bar [=] meters
-w = 0.0508  # width of the rails [=] meters
-h = 0.003175  # height of rail [=] meters
-l = 1  # length of the rails [=] meters
-lp = 0.00635  # length of projectile [=] meters
-hp = 0.00635  # height of projectile [=] meters
-lc = 0.3048  # length of conductor (both leads added) [=] meters
-angle = 0  # angle of launch (from ground) [=] degrees
-initial_velocity = 1  # meters per second
 
 # Capacitor
 capacitance = 1800  # [=] farads
 esr = 0.00016  # [=] ohms
 initial_voltage = 2.85  # volts
 
-# Physical constants
+
+# Railgun, projectile, leads, construction
+w = 0.0508  # width of the rails [=] meters
+h = 0.003175  # height of rail [=] meters
+l = 1  # length of the rails [=] meters
+d = 0.00635  # separation of the rails and width of the bar [=] meters
+
+lp = 0.00635  # length of projectile [=] meters
+hp = 0.00635  # height of projectile [=] meters
+
+lc = 0.3048  # length of conductor (both leads added) [=] meters
+connection_resistance = 0
+
+angle = 0  # angle of launch (from ground) [=] degrees
+initial_velocity = 1  # meters per second
+
+
+# Physical constants (should not need to change)
 mu0 = (4 * np.pi) * (10 ** -7)  # magnetic constant [=] newtons per ampere squared
 muR = 0.999994  # relative permeability of copper, very close to permeability of free space (mu0) [=] mu / mu0
 copper_resistivity = 1.68 * (10 ** -8)  # [=] ohms * meters
@@ -28,7 +35,6 @@ mass = lp * d * hp * copper_density  # [=] kilograms
 inductance_gradient = ((4 * mu0 * muR * (lp + w)) / h)  # inductance constant (multiplies with position for inductance)
 resistance_gradient = 2 * copper_resistivity / (w * h)  # resistance of rails (multiplies with position for resistance)
 projectile_resistance = copper_resistivity * d / (hp * lp)
-connection_resistance = 0
 weight = mass * 9.81
 friction_force = friction_coefficient * weight * np.cos(np.radians(angle))  # [=] newtons
 static_resistance = esr + projectile_resistance + connection_resistance  # [=] ohms
