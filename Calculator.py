8822d8c85878deb53a488a63a142b583ad99fe98
Mitchell Blaha <99313238+WhiteRabbit2006@users.noreply.github.com>
2023-02-28 13:41:04 -0500

    Update Calculator.py
    Minor changes

--- Calculator.py
+++ Calculator.py
@@ -1,13 +1,12 @@
 import numpy as np
-import scipy.integrate
-from scipy.integrate import odeint, quadrature
+from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 # Railgun and projectile construction
-D = 0.00635  # separation of the rails and width of the bar [=] meters
+d = 0.00635  # separation of the rails and width of the bar [=] meters
 w = 0.0508  # width of the rails [=] meters
 h = 0.003175  # height of rail [=] meters
-L = 1  # length of the rails [=] meters
+l = 1  # length of the rails [=] meters
 lp = 0.00635  # length of projectile [=] meters
 hp = 0.00635  # height of projectile [=] meters
 lc = 0.3048  # length of conductor (both leads added) [=] meters
@@ -25,10 +24,10 @@ muR = 0.999994  # relative permeability of copper, very close to permeability of
 copper_resistivity = 1.68 * (10 ** -8)  # [=] ohms * meters
 copper_density = 8950  # [=] kg per cubic meter
 friction_coefficient = 0.2  # friction coefficient of copper [=]
-mass = lp * D * hp * copper_density  # [=] kilograms
+mass = lp * d * hp * copper_density  # [=] kilograms
 inductance_gradient = ((4 * mu0 * muR * (lp + w)) / h)  # inductance constant (multiplies with position for inductance)
 resistance_gradient = 2 * copper_resistivity / (w * h)  # resistance of rails (multiplies with position for resistance)
-projectile_resistance = copper_resistivity * D / (hp * lp)
+projectile_resistance = copper_resistivity * d / (hp * lp)
 connection_resistance = 0
 weight = mass * 9.81
 friction_force = friction_coefficient * weight * np.cos(np.radians(angle))  # [=] newtons
@@ -48,7 +47,7 @@ def EMF(velocity):  # returns resistance due to EMF [=] ohms
 def dydt(y, t):
     position, velocity, current, current_rate = y
 
-    if position < L:
+    if position < l:
         acceleration = (1 / 2 * inductance_gradient * current ** 2 - friction_force) / mass
         current_rate_rate = -1 * (
                 -1 / capacitance * current + static_resistance * current_rate + resistance_gradient * (
@@ -70,8 +69,8 @@ y0 = [0.0, initial_velocity, 0, initial_current_rate]
 y1 = odeint(dydt, y0, time)
 
 # plt.plot(time, y1[:, 0], 'b', label='position')
-# plt.plot(time, y1[:, 1], 'r', label='velocity')
-plt.plot(time, y1[:, 2], 'g', label='current')
+plt.plot(time, y1[:, 1], 'r', label='velocity')
+# plt.plot(time, y1[:, 2], 'g', label='current')
 # plt.plot(time, y1[:, 3], 'o' label='current_rate')
 plt.legend(loc='best')
 plt.xlabel('t')
